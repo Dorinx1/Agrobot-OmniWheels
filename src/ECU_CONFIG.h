b0ad9e2b0e76889c4366593e47ce9ee876393444
Dorinx1 <Dorinx1>
2024-04-09 13:13:20 +0300

    3_Commit

--- src/ECU_CONFIG.h
+++ src/ECU_CONFIG.h
@@ -3,6 +3,19 @@
 
 /*Electronic Config Unit*/
 
+/*Define ECU Config*/
+#define ECU_CONFIG
+
+/*Define ECU Dispositive*/
+#define USE_CAN_SETUP
+#define USE_STERING_SETUP
+
+#elif ECU_CONFIG == USE_CAN_SETUP
+#include "DD_CAN_MCP/dd_can_mcp.h"
+
+#elif ECU_CONFIG == USE_STERING_SETUP
+#include "DD_STERING/dd_stering.h"
+#endif
 
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,14 @@
 
 #define CAN_SEND_REC (5000 / SYS_TICK)
 #define CAN_RECV_REC (1 / SYS_TICK)
-#define CAN_PRINT_REC (2000/SYS_TICK)
+#define CAN_PRINT_REC (2000/SYS_TICK)  //is not used
+#define STERING_PRINT_REC (100/SYS_TICK)
 
 /*Private variable*/
 int16_t send_rec_cnt = CAN_SEND_REC;
 int8_t recv_rec_cnt = CAN_RECV_REC;
 int16_t print_rec_cnt = CAN_PRINT_REC;
+int32_t stering_rec_cnt = STERING_PRINT_REC;
 
 /*Declare Setup*/
 void setup() 
@@ -38,14 +40,22 @@ void setup()
 void loop()
 {
    if (--send_rec_cnt <= 0 )            // algorit to transmite data pe  ( X )ms 
-  {// send data per 500ms
+  {// send data per 50ms
     dd_can_send_loop();
     send_rec_cnt = CAN_SEND_REC;
   }
+
   if (--recv_rec_cnt <= 0)
-  { // Send data per 1 ms
+  { // Send data per 0,01 ms
     dd_can_recv_loop();
     recv_rec_cnt = CAN_RECV_REC;
   }
+
+  if (--stering_rec_cnt <=0)
+  {// send data per 0,10 ms
+    dd_stering_loop();
+    stering_rec_cnt = STERING_PRINT_REC;
+  }
+
 }  
 
